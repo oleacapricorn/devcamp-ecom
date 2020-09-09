0032346fdf8303d0e15fe73bb758db0a128c5f67
oleacapricorn <fomaolga@gmail.com>
2020-09-10 01:03:52 +0300

    finished header feature

--- src/components/headernavbar/header.js
+++ src/components/headernavbar/header.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
+import history from '../../history';
+
 
 
 class Header extends Component {
@@ -12,7 +14,7 @@ class Header extends Component {
                     {
                         this.props.headerLinks.map((link, index) => {
                             return (
-                                <a className='header__link' key={index} onClick={() => console.log('trying to switch tab')}>
+                                <a className='header__link' key={index} onClick={() => history.push(link.path)}>
                                     {link.title}
                                 </a>
                             )
