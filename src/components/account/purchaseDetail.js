6c7b9b25c6da5e660f50c1ef466aa3a6f6cdc955
oleacapricorn <fomaolga@gmail.com>
2020-09-13 20:25:13 +0300

    implemented set purchase detail action creator and reducer

--- src/components/account/purchaseDetail.js
+++ src/components/account/purchaseDetail.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+
+function PurchaseDetailLabel({className, title, value}) {
+    return (
+        <div className={`${className} purchase-detail-lable`}>
+            <div className='purchase-detail-lable__title'>{title}</div>
+            <div className='purchase-detail-lable__value'>{value}</div>
+        </div>
+    )
+}
+
 class PurchaseDetail extends Component {
     render() {
-        const { className } = this.props;
-
+        const { className, orderDate, orderNmber, user } = this.props;
+        const { name, shippingAddress, total, creditCard } = user;
         return (
             <div className={`${className} purchase-detail`}>
-                purchase detail goes here
+                <PurchaseDetailLabel
+                    className='purchase-detail__order-number'
+                    title='Order Number'
+                    value={orderNmber} />
+                <PurchaseDetailLabel
+                    className='purchase-detail__order-date'
+                    title='Order Number'
+                    value={orderDate} />
+                <PurchaseDetailLabel
+                    className='purchase-detail__shipping'
+                    title='Shipping Address'
+                    value={`${name}\n${shippingAddress}`} />
+                <PurchaseDetailLabel
+                    className='purchase-detail__total'
+                    title='Total'
+                    value={total} />
+                <PurchaseDetailLabel
+                    className='purchase-detail__credit-card'
+                    title='Credit Card'
+                    value={creditCard} />
+                
             </div>
         )
     }
 }
+
+function mapStateToProps(state) {
+    const { purchaseDetail } = state.user;
+    return {
+        ...purchaseDetail
+    }
+}
+
+PurchaseDetail = connect(mapStateToProps)(PurchaseDetail);
 
 export default PurchaseDetail; 
